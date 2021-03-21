122310597d932b1fbd353d68b1246aecef6f09c8
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2021-03-21 03:07:36 -0400

    UI EXPERIMENTS

--- 01_JqueryUITesting/js/script.js
+++ 01_JqueryUITesting/js/script.js
@@ -7,30 +7,63 @@ Experiment with jQueryUI
 //$(`#prisoner`).draggable();
 
 //separate functions by comma!
-$(`#prisoner`).draggable({
-  //axis: `x`,
-  containment: `#prison`,
-  //anon function that calls when the dragging starts
-  start:function(){
-    $(`#prisoner`).css(`text-decoration`, `underline`);
-    $(`#prisoner`).css(`color`, `red`);
-    //animates the text to blue in 750miliseconds
-    $(this).animate({
-      "color": `#4444ff`
-    }, 750);
-  },
-  stop:function(){
-    //this refers to #prisoner
-    $(this).css(`text-decoration`, `none`);
-    $(this).css(`color`, `black`);
-    $(this).animate({
-      "color": `0000ff`
-    }, 750);
+
+$(`#escape-tunnel`).hide();
+
+$(`#intro-dialog`).dialog({
+  modal: true,
+  resizable: false,
+  buttons: {
+    "Imagination": function() {
+      $(this).dialog(`close`);
+      $(`#prisoner`).draggable(`option`,`containment`,`none`);
+    },
+    "tunnel": function(){
+      $(this).dialog(`close`);
+      $(`#escape-tunnel`).show();
+    }
   }
 });
+
+
+$(`#prisoner`).effect({
+  effect: `shake`,
+  duration: 2000,
+  times:15,
+  distance:7,
+  complete: makePrisonerDraggable
+})
+
+function makePrisonerDraggable(){
+  $(`#prisoner`).draggable({
+    //axis: `x`,
+    containment: `#prison`,
+    //anon function that calls when the dragging starts
+    start:function(){
+      $(`#prisoner`).css(`text-decoration`, `underline`);
+      //$(`#prisoner`).css(`color`, `red`);
+      //animates the text to blue in 750miliseconds
+      $(this).animate({
+        "color": `#4444ff`
+      }, 750);
+    },
+    stop:function(){
+      //this refers to #prisoner
+      $(this).css(`text-decoration`, `none`);
+      //animation can be
+      $(this).animate({
+        "color": `#000000`
+      }, 750);
+    }
+  });
+}
 
 $(`#escape-tunnel`).droppable({
   drop: function(event, ui){
     ui.draggable.remove();
+    $(this).hide({
+      effect: `blind`,
+      duration:500
+    });
   }
 })
